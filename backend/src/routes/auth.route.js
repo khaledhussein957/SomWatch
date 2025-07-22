f551bef503976d1178fc41a55d8e67be911cb964
khaled29-coder <khaledhussein957@gmail.com>
2025-07-22 07:01:35 +0300

    remove the googleOauth

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,8 +1,7 @@
 import express from "express";
 
-import { login, register, callBack, forgotPassword, resetPassword } from "../controllers/auth.controller.js";
+import { login, register, forgotPassword, resetPassword } from "../controllers/auth.controller.js";
 
-import getGoogleAuthURL from "../utils/googleOauthUrl.js";
 
 const router = express.Router();
 
@@ -12,13 +11,5 @@ router.post("/register", register);
 
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
-
-// Google OAuth routes
-router.get("/google", (req, res) => {
-  const url = getGoogleAuthURL();
-  res.redirect(url);
-});
-
-router.get("/google/callback", callBack);
 
 export default router;
